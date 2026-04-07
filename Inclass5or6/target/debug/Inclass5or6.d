c0be564fb96aa0ce6b7534f50464e6104f2bd3cd
Normano1101 <normanbm123@gmail.com>
2026-04-07 17:24:02 +0000

    inclas 5 done

---
+++ Inclass5or6/target/debug/Inclass5or6.d
@@ -0,0 +1,1 @@
+/workspaces/spring2026/Inclass5or6/target/debug/Inclass5or6: /workspaces/spring2026/Inclass5or6/src/main.rs
---
+++ Inclass5or6/target/debug/deps/Inclass5or6-0acb512a5969e797.d
@@ -0,0 +1,5 @@
+/workspaces/spring2026/Inclass5or6/target/debug/deps/Inclass5or6-0acb512a5969e797.d: src/main.rs
+
+/workspaces/spring2026/Inclass5or6/target/debug/deps/Inclass5or6-0acb512a5969e797: src/main.rs
+
+src/main.rs:
